7f654f0cd34c05bac210e0609380ed2b3765c088
Oliver <softwaredevdad@gmail.com>
2025-09-06 15:16:50 +0200

    add testimionals sections and some css, add orders component to display users orders

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import BestSeller from "../components/BestSeller";
 import BottomBanner from "../components/BottomBanner";
 import NewsLetter from "../components/NewsLetter";
 import Footer from "../components/Footer";
+import Testimonials from "../components/Testimonials";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Categories />
       <BestSeller />
       <BottomBanner />
+      <Testimonials />
       <NewsLetter />
     </div>
   );
