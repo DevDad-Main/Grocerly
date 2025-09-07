335af42809dd7cd1429d8df90631d3f98a583178
Oliver <softwaredevdad@gmail.com>
2025-09-07 23:11:54 +0200

    add address schema and controller file, change user name property to be first name and lastname, and use the user schema as a ref on our address

---
+++ server/src/model/Address.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  street: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  zipcode: {
+    type: String, // Saving it as a string as multiple addresses can have a mix of numbers and letters
+    required: true,
+  },
+  country: { type: String, required: true },
+  phone: { type: Number, required: true }, // leaving this here as at the minute i dont like the idea of it being on the user, may change this later
+});
+
+export const Address = mongoose.model("Address", addressSchema);
