65c77fc045492546bda15d8c2f67059a309179a0
Oliver <softwaredevdad@gmail.com>
2025-09-07 14:47:58 +0200

    add authentication middleware ofr WT

---
+++ server/src/middlewares/authentication.middlewares.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/User.models.js";
+
+const isAuthenticated = async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return res.json({ success: false, message: "Not Auhtorized" });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decodedToken?._id);
+
+    if (!user) {
+      return res.json({ success: false, message: "Unauthorized" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ success: false, message: "Unauthorized" });
+  }
+};
